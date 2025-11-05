1b984c87c109d8ca17dd7372309422e26f5ad37e
maheen <maheenzuhra@gmail.com>
2025-11-05 16:15:43 +0500

    solve one question more

--- js problem day 2/script.js
+++ js problem day 2/script.js
@@ -50,3 +50,52 @@ function findNumber(num){
 }
 console.log(findNumber(20));
 
+// Q3 calculate eletricity bill unit 100 charge 5rs unit 200 charge 7rs onward 10
+
+function calculateBill(units){
+    let eBill = 0;
+    if(units <= 100){
+        eBill = units * 5;
+    }
+    else if(units <= 200){
+        eBill = (100 * 5) + (units - 100) *7;
+    }
+    else{
+        eBill = (100 * 5)+(100 * 7)+(units - 200)*10;
+    }
+    return eBill;
+}
+console.log(calculateBill(200));
+// loop se bhe ho skta hy
+
+// Q4 check the letter is vowel or consunant
+
+function checkVowel(char = '0'){
+    if("aeiou".includes(char.toLowerCase()) && char !== ""){
+        return "its a vowel";
+    }
+    else if (/[a-z]/.test(char.toLowerCase())){
+        return "its a consunant";
+    }
+    return " not a letter"
+}
+console.log(checkVowel('5'));
+// aski cid se bhe ho skta charCode()
+
+// Q4 check if the year is leap year or not.
+ function leapYear(year){
+    if (year % 400 === 0 ||(year % 100 !==0 && year % 4 === 0)){
+        return true;
+    }
+    return false;
+ }
+console.log(leapYear(2024));
+
+// home work 
+// Q1 check if the char is upper lower case or special charaater
+// Q2 check trinagle type using angle side
+// Q3 calculate incom tax slab
+// Q4
+
+
+
