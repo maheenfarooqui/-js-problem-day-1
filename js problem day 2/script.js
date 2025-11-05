e239bb7a6dc354953c15b60bda7617075cc3b869
maheen <maheenzuhra@gmail.com>
2025-11-05 21:26:11 +0500

    solve all home work

--- js problem day 2/script.js
+++ js problem day 2/script.js
@@ -106,8 +106,55 @@ function charCheck(charCase){
 console.log(charCheck("^"));
 
 // Q2 check trinagle type using angle side
+// Equilateral → all sides equal
+
+//  Isosceles → any 2 sides equal
+
+//  Scalene → all sides different
+// Equilateral → all 3 angles are equal (60°, 60°, 60°)
+
+//  Isosceles → any 2 angles are equal
+
+//  Scalene → all angles are different
+
+function angleCheck(a,b,c,x,y,z){
+    if((x+y+z !==180) && (a + b <= c || a + c <= b || b + c <= a )){
+        return "its not a valid triangle"
+
+    }
+    else if((a === b && b===c) && (x===y && y===z)){
+        return "it Equilateral";
+    }
+    else if((a===b || b === c) &&( x ===y || y ===z )){
+        return "its  Isosceles";
+    }
+
+    return "its Scalene";
+
+
+}
+console.log(angleCheck(3, 4, 5, 90, 60, 30)); // Scalene
 
 // Q3 calculate incom tax slab
+
+function checkIncomTax(tax){
+    let incomTax =0 ;
+    if(tax <=12000 ){
+        return "no incometax";
+    }
+    else if(tax <= 25000){
+        incomTax = tax * 0.05;
+    }
+    else if(tax <=35000){
+        incomTax= tax * 0.10;
+    }
+    else{
+        incomTax = tax * 0.15;
+    }
+    return incomTax;
+}
+console.log(checkIncomTax(35000));
+
 
 
 
